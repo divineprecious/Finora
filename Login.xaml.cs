c1aa93592f258d9f52fed3b1dc90662ca451256f
divineprecious <divinepreciousesue@gmail.com>
2025-04-16 16:16:50 -0500

    Added navigation and dummy log in functionality

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -6,4 +6,26 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void OnLoginClicked(object sender, EventArgs e) 
+	{
+		string email = userEmail.Text;
+		string password = userPassword.Text;
+
+		//Basic hardcoded example
+		if (email == "user@example.com" &&  password == "password123")
+		{
+			//Navigate to Shell-based app
+			Application.Current.MainPage = new AppShell();
+		}
+		else 
+		{
+			DisplayAlert("Login Failed", "Incorrect email or password.", "OK");
+		}
+	}
+
+	private void OnCreateClicked(object sender, EventArgs e) 
+	{
+		Application.Current.MainPage = new Signup();
+	}
 }
